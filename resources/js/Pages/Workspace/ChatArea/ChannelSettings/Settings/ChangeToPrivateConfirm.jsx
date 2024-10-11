ccfbc9beacaeac5ed0a02d7f483ec25455a38a9e
Duke Lee <minhduc8a2.1@gmail.com>
2024-10-11 21:55:41 +0700

    ChangeToPrivateConfirm customedDialog

--- resources/js/Pages/Workspace/ChatArea/ChannelSettings/Settings/ChangeToPrivateConfirm.jsx
+++ resources/js/Pages/Workspace/ChatArea/ChannelSettings/Settings/ChangeToPrivateConfirm.jsx
@@ -1,17 +1,16 @@
 import Button from "@/Components/Button";
-import OverlayLoadingSpinner from "@/Components/Overlay/OverlayLoadingSpinner";
+import CustomedDialog from "@/Components/CustomedDialog";
 
-import OverlayNotification from "@/Components/Overlay/OverlayNotification";
 import useErrorHandler from "@/helpers/useErrorHandler";
-import { router, useForm, usePage } from "@inertiajs/react";
+
 import React, { useMemo } from "react";
 import { useState } from "react";
 import { FaLock } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 export default function ChangeToPrivateConfirm({ channelName }) {
-    const {  workspace } = useSelector(state=>state.workspace)
-    const {channelId} = useParams()
+    const { workspace } = useSelector((state) => state.workspace);
+    const { channelId } = useParams();
 
     const { channels } = useSelector((state) => state.channels);
 
@@ -27,11 +26,9 @@ export default function ChangeToPrivateConfirm({ channelName }) {
             : "";
     }, [channel]);
 
-    const [success, setSuccess] = useState(false);
-
     const [processing, setProcessing] = useState(false);
     const handleError = useErrorHandler();
-
+    const [isOpen, setIsOpen] = useState(false);
     function onSubmit(e) {
         e.preventDefault();
         setProcessing(true);
@@ -41,47 +38,34 @@ export default function ChangeToPrivateConfirm({ channelName }) {
             })
             .catch(handleError)
             .finally(() => {
-                setSuccess(true);
+                setIsOpen(false);
                 setProcessing(false);
             });
     }
     return (
-        <OverlayNotification
-            success={success}
-            className="p-3"
-            title={`Change to a ${
-                channel.type == "PUBLIC" ? "private" : "public"
-            } channel?`}
-            buttonNode={
-                <Button
-                    className=" bg-transparent w-full !rounded-none    py-4 border-none !justify-start"
-                    onClick={() => {
-                        setSuccess(false);
-                    }}
-                >
-                    <div className="flex items-center gap-x-2">
-                        {channel.type != "PUBLIC" ? (
-                            <span className="text-lg">#</span>
-                        ) : (
-                            <FaLock className="text-sm" />
-                        )}{" "}
-                        Change to a{" "}
-                        {channel.type == "PUBLIC" ? "private" : "public"}{" "}
-                        channel
-                    </div>
-                </Button>
-            }
-            submitButtonNode={
-                <Button className="!bg-danger-500  relative" onClick={onSubmit}>
-                    {processing ? <OverlayLoadingSpinner /> : ""}
-                    <span className={processing ? "invisible" : ""}>
-                        Change to{" "}
-                        {channel.type == "PUBLIC" ? "Private" : "Public"}
-                    </span>
-                </Button>
-            }
-        >
-            <div className="">
+        <>
+            <Button
+                className=" bg-transparent w-full !rounded-none    py-4 border-none !justify-start"
+                onClick={() => {
+                    setIsOpen(true);
+                }}
+            >
+                <div className="flex items-center gap-x-2">
+                    {channel.type != "PUBLIC" ? (
+                        <span className="text-lg">#</span>
+                    ) : (
+                        <FaLock className="text-sm" />
+                    )}{" "}
+                    Change to a{" "}
+                    {channel.type == "PUBLIC" ? "private" : "public"} channel
+                </div>
+            </Button>
+            <CustomedDialog isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                <CustomedDialog.Title>
+                    {`Change to a ${
+                        channel.type == "PUBLIC" ? "private" : "public"
+                    } channel?`}
+                </CustomedDialog.Title>
                 <div>
                     Keep in mind that when you make{" "}
                     {channel.type == "PUBLIC" ? (
@@ -105,7 +89,19 @@ export default function ChangeToPrivateConfirm({ channelName }) {
                             : "If you make the channel private again, it’ll be visible to anyone who’s joined the channel up until that point."}
                     </li>
                 </ul>
-            </div>
-        </OverlayNotification>
+
+                <CustomedDialog.ActionButtons
+                    btnName2={
+                        <>
+                            Change to{" "}
+                            {channel.type == "PUBLIC" ? "Private" : "Public"}
+                        </>
+                    }
+                    loading={processing}
+                    onClickBtn2={onSubmit}
+                    type={channel.type != "PUBLIC" ? "" : "danger"}
+                />
+            </CustomedDialog>
+        </>
     );
 }
